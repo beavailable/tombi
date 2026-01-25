87d7c5639982c46e59a9fdc014b328fd181ef9f0
beavailable <beavailable@proton.me>
2026-01-25 13:06:01 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
+NEW_VERSION=''
+NEW_REVISION=''
+
 # $@: arguments
 _curl() {
     local retries
@@ -17,18 +20,46 @@ _curl() {
         sleep $((retries * 5))
     done
 }
-# $1: version
-download_binaries() {
+fetch_new_version() {
+    local version revision tags tag
+    read -r version revision <<<$(sed -nE '1s/^\S+ \((\S+)-(\S+)\) .+$/\1 \2/p' debian/changelog)
+    tags=$(_curl "https://api.github.com/repos/tombi-toml/tombi/tags" | sed -nE 's/^\s+"name": "v(\S+)",$/\1/p')
+    for tag in $tags; do
+        if [[ "$tag" == "$version" ]]; then
+            break
+        fi
+        if dpkg --compare-versions "$tag" gt "$version"; then
+            NEW_VERSION="$tag"
+            break
+        fi
+    done
+
+    if [[ -n "$NEW_VERSION" ]]; then
+        NEW_REVISION='1'
+    elif [[ "${ENV_FORCE_RELEASE:-}" == 'true' ]]; then
+        NEW_VERSION="$version"
+        NEW_REVISION=$((revision + 1))
+    fi
+}
+download_new_version() {
+    local f
+    for f in "tombi-cli-$NEW_VERSION-x86_64-unknown-linux-musl.gz" "tombi-cli-$NEW_VERSION-aarch64-unknown-linux-musl.gz"; do
+        _curl -O "https://github.com/tombi-toml/tombi/releases/download/v$NEW_VERSION/$f"
+    done
+}
+prepare_new_version() {
     local f bin
-    for f in "tombi-cli-$1-x86_64-unknown-linux-musl.gz" "tombi-cli-$1-aarch64-unknown-linux-musl.gz"; do
-        _curl -O "https://github.com/tombi-toml/tombi/releases/download/v$1/$f"
+    for f in tombi-*.gz; do
+        [[ -f "$f" ]] || return 1
         gzip -d "$f"
         bin="${f%.gz}"
         chmod +x "$bin"
-        if [[ "$bin" == tombi-cli-$1-x86_64* ]]; then
+        if [[ "$bin" == tombi-cli-$NEW_VERSION-x86_64* ]]; then
             mv "$bin" tombi_amd64
-        else
+        elif [[ "$bin" == tombi-cli-$NEW_VERSION-aarch64* ]]; then
             mv "$bin" tombi_arm64
+        else
+            return 1
         fi
     done
     mkdir completions
@@ -36,48 +67,31 @@ download_binaries() {
     ./tombi_amd64 completion zsh >completions/_tombi
     ./tombi_amd64 completion fish >completions/tombi.fish
 }
+release_new_version() {
+    local changelog debian_version user email
+    debian_version="$NEW_VERSION-$NEW_REVISION"
+    changelog=$(cat debian/changelog)
+    {
+        echo "tombi ($debian_version) unstable; urgency=medium"
+        echo
+        echo '  * New release.'
+        echo
+        echo " -- beavailable <beavailable@proton.me>  $(date '+%a, %d %b %Y %H:%M:%S %z')"
+        echo
+        echo "$changelog"
+    } >debian/changelog
 
-read -r version revision <<<$(sed -nE '1s/^\S+ \((\S+)-(\S+)\) .+$/\1 \2/p' debian/changelog)
-new_version=''
-tags=$(_curl "https://api.github.com/repos/tombi-toml/tombi/tags" | sed -nE 's/^\s+"name": "v(\S+)",$/\1/p')
-for tag in $tags; do
-    if [[ "$tag" == "$version" ]]; then
-        break
-    fi
-    if dpkg --compare-versions "$tag" gt "$version"; then
-        new_version="$tag"
-        break
-    fi
-done
+    user='github-actions[bot]'
+    email='41898282+github-actions[bot]@users.noreply.github.com'
+    git -c user.name="$user" -c user.email="$email" commit -am "Release $debian_version" --author "$GITHUB_ACTOR <$GITHUB_ACTOR_ID+$GITHUB_ACTOR@users.noreply.github.com>"
+    git -c user.name="$user" -c user.email="$email" tag "$debian_version" -am "Release $debian_version"
+    git push origin --follow-tags --atomic
+}
 
-if [[ -n "$new_version" ]]; then
-    download_binaries "$new_version" || exit 0
-    new_version="$new_version-1"
-elif [[ "${TOMBI_FORCE_RELEASE:-}" == 'true' ]]; then
-    download_binaries "$version" || exit 0
-    new_version="$version-$((revision + 1))"
-else
-    exit 0
-fi
-
-changelog=$(cat debian/changelog)
-{
-    echo "tombi ($new_version) unstable; urgency=medium"
-    echo
-    echo '  * New release.'
-    echo
-    echo " -- beavailable <beavailable@proton.me>  $(date '+%a, %d %b %Y %H:%M:%S %z')"
-    echo
-    echo "$changelog"
-} >debian/changelog
-
-user='github-actions[bot]'
-email='41898282+github-actions[bot]@users.noreply.github.com'
-git -c user.name="$user" -c user.email="$email" commit -am "Release $new_version" --author "$GITHUB_ACTOR <$GITHUB_ACTOR_ID+$GITHUB_ACTOR@users.noreply.github.com>"
-git -c user.name="$user" -c user.email="$email" tag "$new_version" -am "Release $new_version"
-git push origin --follow-tags --atomic
-
-git add tombi_* completions
-git -c user.name="$user" -c user.email="$email" commit -m 'Add files'
+fetch_new_version
+[[ -n "$NEW_VERSION" ]] || exit 0
+download_new_version || exit 0
+prepare_new_version
+release_new_version
 
 echo "release=true" >>$GITHUB_OUTPUT
